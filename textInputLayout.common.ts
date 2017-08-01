9d4a4e0aef340c24495284c937ab57239e0729a1
Hristo Hristov <Hristo.Hristov@telerik.com>
2017-08-01 13:53:16 +0300

    Migrate to nativescript 3.0 Code refactoring and optimizations

--- textInputLayout.common.ts
+++ textInputLayout.common.ts
@@ -1,22 +1,38 @@
-import definition = require('textInputLayout');
-import {View, Property} from "tns-core-modules/ui/core/view";
-import {PropertyMetadataSettings} from "tns-core-modules/ui/core/dependency-observable";
-import {TextView} from 'tns-core-modules/ui/text-view';
-import {TextField} from 'tns-core-modules/ui/text-field';
+import { TextInputLayout as TextInputLayoutDefinition } from './textInputLayout';
 import { Color } from "tns-core-modules/color";
+import { View, Property } from "tns-core-modules/ui/core/view";
 
-const TIL = 'TextInputLayout';
+export { Property, Color };
 
-
-export class TextInputLayout extends View implements definition.TextInputLayout {
-
-    constructor() {
-        super();
-    }
+export class TextInputLayout extends View implements TextInputLayoutDefinition {
 
     // common
     public hint: string;
     public error: string;
+
+    //ANDROID ONLY
+    public hintAnimationEnabled?: boolean;
+    public hintTextAppearance?: string;
+    public counterEnabled?: boolean;
+    public errorEnabled?: boolean;
+
+    //IOS Only
+    public title?: string;
+    public tintColor?: Color;
+    public lineColor?: Color;
+    public selectedTintColor?: Color;
+    public selectedLineColor?: Color;
+    public selectedTitleColor?: Color;
+    public lineHeight?: number;
+    public selectedLineHeight?: number;
+    public errorColor?: Color;
+    public iconColor?: Color;
+    public selectedIconColor?: Color;
+    public iconFont?: string;
+    public iconText?: string;
+    public iconMarginBottom?: number;
+    public iconMarginLeft?: number;
+    public iconRotationDegrees?: number;
 
     /* GETTERS/SETTERS for other property accessors implemented individually in ios/android components as they're so different */
 }
@@ -27,139 +43,11 @@ export const errorProperty = new Property<TextInputLayout, string>({
     name: "error",
     affectsLayout: true
 });
-// errorProperty.register(TextInputLayout);
+errorProperty.register(TextInputLayout);
 
 // NOTE: This is 'placeholder' for ios
 export const hintProperty = new Property<TextInputLayout, string>({
     name: "hint",
     affectsLayout: true
 });
-// hintProperty.register(TextInputLayout);
-
-/*** android-only properties ***/
-export const hintAnimationEnabledProperty = new Property<TextInputLayout, boolean>({
-    name: "hintAnimationEnabled"
-});
-hintAnimationEnabledProperty.register(TextInputLayout);
-
-export const hintTextAppearanceProperty = new Property<TextInputLayout, string>({
-    name: "hintTextAppearance"
-});
-hintTextAppearanceProperty.register(TextInputLayout);
-
-export const counterEnabledProperty = new Property<TextInputLayout, boolean>({
-    name: "counterEnabled",
-    affectsLayout: true
-});
-counterEnabledProperty.register(TextInputLayout);
-
-export const errorEnabledProperty = new Property<TextInputLayout, boolean>({
-    name: "errorEnabled",
-    affectsLayout: true
-});
-errorEnabledProperty.register(TextInputLayout);
-
-/*** ios-only properties ***/
-
-// note: this is the floating label value 
-export const titleProperty = new Property<TextInputLayout, string>({
-    name: "title",
-    affectsLayout: true
-});
-// titleProperty.register(TextInputLayout);
-
-export const tintColorProperty = new Property<TextInputLayout, Color>({
-    name: "tintColor",
-    equalityComparer: Color.equals,
-    valueConverter: (v) => new Color(v)
-});
-// tintColorProperty.register(TextInputLayout);
-
-export const lineColorProperty = new Property<TextInputLayout, Color>({
-    name: "lineColor",
-    equalityComparer: Color.equals,
-    valueConverter: (v) => new Color(v)
-});
-// lineColorProperty.register(TextInputLayout);
-
-export const selectedTitleColorProperty = new Property<TextInputLayout, Color>({
-    name: "selectedTitleColor",
-    equalityComparer: Color.equals,
-    valueConverter: (v) => new Color(v)
-});
-// selectedTitleColorProperty.register(TextInputLayout);
-
-export const selectedLineColorProperty = new Property<TextInputLayout, Color>({
-    name: "selectedLineColor",
-    equalityComparer: Color.equals,
-    valueConverter: (v) => new Color(v)
-});
-// selectedLineColorProperty.register(TextInputLayout);
-
-export const lineHeightProperty = new Property<TextInputLayout, number>({
-    name: "lineHeight",
-    affectsLayout: true,
-    valueConverter: (v) => Number(v)
-});
-// lineHeightProperty.register(TextInputLayout);
-
-export const selectedLineHeightProperty = new Property<TextInputLayout, number>({
-    name: "selectedLineHeight",
-    affectsLayout: true,
-    valueConverter: (v) => Number(v)
-});
-// selectedLineHeightProperty.register(TextInputLayout);
-
-export const errorColorProperty = new Property<TextInputLayout, Color>({
-    name: "errorColor",
-    equalityComparer: Color.equals,
-    valueConverter: (v) => new Color(v)
-});
-// errorColorProperty.register(TextInputLayout);
-
-export const iconColorProperty = new Property<TextInputLayout, Color>({
-    name: "iconColor",
-    equalityComparer: Color.equals,
-    valueConverter: (v) => new Color(v)
-});
-// iconColorProperty.register(TextInputLayout);
-
-export const selectedIconColorProperty = new Property<TextInputLayout, Color>({
-    name: "selectedIconColor",
-    equalityComparer: Color.equals,
-    valueConverter: (v) => new Color(v)
-});
-// selectedIconColorProperty.register(TextInputLayout);
-
-export const iconFontProperty = new Property<TextInputLayout, UIFont>({
-    name: "iconFont",
-    affectsLayout: true
-});
-// iconFontProperty.register(TextInputLayout);
-
-export const iconTextProperty = new Property<TextInputLayout, string>({
-    name: "iconText",
-    affectsLayout: true
-});
-// iconTextProperty.register(TextInputLayout);
-
-export const iconMarginBottomProperty = new Property<TextInputLayout, number>({
-    name: "iconMarginBottom",
-    affectsLayout: true,
-    valueConverter: (v) => Number(v)
-});
-// iconMarginBottomProperty.register(TextInputLayout);
-
-export const iconMarginLeftProperty = new Property<TextInputLayout, number>({
-    name: "iconMarginLeft",
-    affectsLayout: true,
-    valueConverter: (v) => Number(v)
-});
-// iconMarginLeftProperty.register(TextInputLayout);
-
-export const iconRotationDegreesProperty = new Property<TextInputLayout, number>({
-    name: "iconRotationDegrees",
-    affectsLayout: true,
-    valueConverter: (v) => Number(v)
-});
-// iconRotationDegreesProperty.register(TextInputLayout);
+hintProperty.register(TextInputLayout);
--- textInputLayout.d.ts
+++ textInputLayout.d.ts
@@ -4,46 +4,63 @@
  *      Implements functionality outline in android's TextInputLayout component
  */
 
+import { Color } from "tns-core-modules/color";
+import { View, Property } from "tns-core-modules/ui/core/view";
 
-declare module 'textInputLayout' {
-    import { Color } from "tns-core-modules/color";
-    import View = require("tns-core-modules/ui/core/view");
-    import {Property} from "tns-core-modules/ui/core/dependency-observable";
+export class TextInputLayout extends View {
+    /**
+     * Native [android TextInputLayout](http://developer.android.com/reference/android/support/design/widget/TextInputLayout.html)
+     */
+    android: any;
 
-    export class TextInputLayout extends View.View {
-         /**
-         * Native [android TextInputLayout](http://developer.android.com/reference/android/support/design/widget/TextInputLayout.html)
-         */
-        android: any;
+    ios: any;
 
-        ios: any;
+    public hint: string;
+    public error: string;
 
-        /**PROPS**/
-        public hint: string;
-        public error: string;
+    //ANDROID ONLY
+    public hintAnimationEnabled?: boolean;
+    public hintTextAppearance?: string;
+    public counterEnabled?: boolean;
+    public errorEnabled?: boolean;
 
-        //ANDROID ONLY
-        public hintAnimationEnabled ?: boolean;
-        public hintTextAppearance ?: string;
-        public counterEnabled ?: boolean;
-        public errorEnabled ?: boolean;
-
-        //IOS Only
-        public title ?: string;
-        public tintColor ?: Color;
-        public lineColor ?: Color;
-        public selectedTintColor ?: Color;
-        public selectedLineColor ?: Color;
-        public selectedTitleColor ?: Color;
-        public lineHeight ?: number;
-        public selectedLineHeight ?: number;
-        public errorColor ?: Color;
-        public iconColor ?: Color;
-        public selectedIconColor ?: Color;
-        public iconFont ?: string;
-        public iconText ?: string;
-        public iconMarginBottom ?: number;
-        public iconMarginLeft ?: number;
-        public iconRotationDegrees ?: number;
-    }
+    //IOS Only
+    public title?: string;
+    public tintColor?: Color;
+    public lineColor?: Color;
+    public selectedTintColor?: Color;
+    public selectedLineColor?: Color;
+    public selectedTitleColor?: Color;
+    public lineHeight?: number;
+    public selectedLineHeight?: number;
+    public errorColor?: Color;
+    public iconColor?: Color;
+    public selectedIconColor?: Color;
+    public iconFont?: string;
+    public iconText?: string;
+    public iconMarginBottom?: number;
+    public iconMarginLeft?: number;
+    public iconRotationDegrees?: number;
 }
+
+export const errorProperty: Property<TextInputLayout, string>;
+export const hintProperty: Property<TextInputLayout, string>;
+export const hintAnimationEnabledProperty: Property<TextInputLayout, boolean>;
+export const hintTextAppearanceProperty: Property<TextInputLayout, string>;
+export const counterEnabledProperty: Property<TextInputLayout, boolean>;
+export const errorEnabledProperty: Property<TextInputLayout, boolean>;
+export const titleProperty: Property<TextInputLayout, string>;
+export const tintColorProperty: Property<TextInputLayout, Color>;
+export const lineColorProperty: Property<TextInputLayout, Color>;
+export const selectedTitleColorProperty: Property<TextInputLayout, Color>;
+export const selectedLineColorProperty: Property<TextInputLayout, Color>;
+export const lineHeightProperty: Property<TextInputLayout, number>;
+export const selectedLineHeightProperty: Property<TextInputLayout, number>;
+export const errorColorProperty: Property<TextInputLayout, Color>;
+export const iconColorProperty: Property<TextInputLayout, Color>;
+export const selectedIconColorProperty: Property<TextInputLayout, Color>;
+export const iconFontProperty: Property<TextInputLayout, UIFont>;
+export const iconTextProperty: Property<TextInputLayout, string>;
+export const iconMarginBottomProperty: Property<TextInputLayout, number>;
+export const iconMarginLeftProperty: Property<TextInputLayout, number>;
+export const iconRotationDegreesProperty: Property<TextInputLayout, number>;
